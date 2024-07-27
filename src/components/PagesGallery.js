bbce295300a7b6ce9e1395a15ee3a54a3a9c49d0
HazemBZ <hazembenbz@gmail.com>
2024-07-27 17:52:57 +0100

    feat: Add loading dots

--- src/components/PagesGallery.js
+++ src/components/PagesGallery.js
@@ -7,50 +7,53 @@ import {
   IconButton,
 } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
+import { useMemo } from "react";
+import styles from "styles.module.css";
 
 const PagesGallery = ({ itemClick, items, loading }) => {
+  const matches = useMemo(() => items.length, [items]);
 
   const LoadedItems = () => {
-    return (
-      items.map((item, i) => (
-        <ImageListItem
-          key={i}
-          style={{
-            border: "2px solid black",
-            overflow: "clip",
-            transition: "0.2s",
-            "&:hover": {
-              transform: "scale(1.1)",
-              width: "400px",
-            },
-          }}
-          onClick={() => itemClick(item)}
-        >
-          <img
-            src={item.img + "?w=164&h=164&fit=crop&auto=format"}
-            srcSet={item.img + "?w=164&h=164&fit=crop&auto=format@dpr=2 x2"}
-            alt={item.title}
-            loading="lazy"
-            width="100%"
-            height="100%"
-          />
-          <ImageListItemBar
-            title={item.title}
-            // subtitle={"subtitle"}
-            actionIcon={
-              <IconButton
-                sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                aria-label={`info about ${item.title}`}
-              >
-                <InfoIcon />
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-      ))
-    )
-  }
-  
+    if (matches === 0) return <h1>🧐 Nothing found</h1>;
+
+    return items.map((item, i) => (
+      <ImageListItem
+        key={i}
+        style={{
+          border: "2px solid black",
+          overflow: "clip",
+          transition: "0.2s",
+          "&:hover": {
+            transform: "scale(1.1)",
+            width: "400px",
+          },
+        }}
+        onClick={() => itemClick(item)}
+      >
+        <img
+          src={item.img + "?w=164&h=164&fit=crop&auto=format"}
+          srcSet={item.img + "?w=164&h=164&fit=crop&auto=format@dpr=2 x2"}
+          alt={item.title}
+          loading="lazy"
+          width="100%"
+          height="100%"
+        />
+        <ImageListItemBar
+          title={item.title}
+          // subtitle={"subtitle"}
+          actionIcon={
+            <IconButton
+              sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+              aria-label={`info about ${item.title}`}
+            >
+              <InfoIcon />
+            </IconButton>
+          }
+        />
+      </ImageListItem>
+    ));
+  };
+
   return (
     <Container
       style={{
@@ -68,16 +71,21 @@ const PagesGallery = ({ itemClick, items, loading }) => {
           justifyContent: "center",
           border: "4px solid black",
           borderRadius: "5px",
+          alignItems: loading ? "center" : "flex-start",
         }}
       >
-        <ImageList
-          sx={{ width: "90%", height: "90%" }}
-          cols={2}
-          rowHeight={164}
-          gap={5}
-        >
-          {loading? (<h1>Loading</h1>) :(<LoadedItems />)}
-        </ImageList>
+        {loading ? (
+          <div className={styles["dot-flashing"]}></div>
+        ) : (
+          <ImageList
+            sx={{ width: "90%", height: "90%" }}
+            cols={2}
+            rowHeight={164}
+            gap={5}
+          >
+            <LoadedItems />
+          </ImageList>
+        )}
       </Box>
     </Container>
   );
