4156a470f2e535c62966e990fa68d7d202901656
HazemBZ <hazembenbz@gmail.com>
2024-08-12 19:36:31 +0100

    feat: Adds select all button

--- src/components/FilesSelector.js
+++ src/components/FilesSelector.js
@@ -1,29 +1,57 @@
-import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+import { Button, Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 import { useState } from "react";
 
 const FilesSelector = ({ metas, handleItemSelection }) => {
-  const [checked, setChecked] = useState(Array(metas.length).fill(false));
+  const [checked, setChecked] = useState(Array(metas.length).fill(true));
 
-  const handleChange = (event, name) => {
-    const id = event.target.id;
+  const handleChange = (id, name) => {
     setChecked((old) => {
-      let new_a = [...old];
-      new_a[id] = !new_a[id];
-      return new_a;
+      let new_l = [...old];
+      new_l[id] = !new_l[id];
+      return new_l;
     });
     handleItemSelection(name);
   };
 
+  const allChecked =
+    checked.length > 0 && checked.reduce((a, b) => a && b, true);
+
+  const handleGroupToggle = () => {
+    if (allChecked) {
+      setChecked(Array(metas.length).fill(false));
+    } else {
+      setChecked(Array(metas.length).fill(true));
+    }
+  };
+
   return (
     <FormGroup style={{ padding: 50 }}>
+      <FormControlLabel
+        control={
+          <Button
+            onClick={handleGroupToggle}
+            style={{
+              padding: "0 0",
+              textDecoration: "underline",
+              borderRadius: "15px",
+            }}
+            color="primary"
+          >
+            <Checkbox checked={allChecked} onChange={handleGroupToggle} />
+            Check all
+          </Button>
+        }
+      />
       {metas.map(({ name }, c) => (
         <FormControlLabel
           id={c}
           key={name + c}
           control={
             <Checkbox
-              checked={checked[c]}
-              onChange={(e) => handleChange(e, name)}
+              checked={Boolean(checked[c])}
+              onChange={(e) => {
+                handleChange(c, name);
+              }}
             />
           }
           label={name}
